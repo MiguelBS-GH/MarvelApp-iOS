a8c44af8a26620e89663a0af9a10d594aad9fc5b
Bram3r <mikelbra99@gmail.com>
2021-07-23 16:44:55 +0200

    Initial Commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Miguel Bragado Sánchez on 23/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
